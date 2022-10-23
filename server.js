bb082f20d58728ba0c0b16a460f2bee2a3c97739
Keegan Gaffney <mcfeegan123@gmail.com>
2022-10-22 19:35:56 -0700

    Server stuff

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const nodemon = require('nodemon');
 const ws = require('ws');
-const port = 3000;
 
-app.use(bodyParser.json());
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-    res.send("Welcome to your server");
-})
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
 
-app.post('/index', (req, res)=>{
-    console.log(req.body.username);
-    console.log(req.body.password);
-});
-
+// Set up a headless websocket server that prints any
+// events that come in.
 const wsServer = new ws.Server({ noServer: true });
-wsServer.on('connection', (socket) => {
-    socket.on('message', (message) => console.log(message));
+wsServer.on('connection', socket => {
+  socket.on('message', message => {
+    console.log("" + message);
+    socket.send(`Hello, you sent -> ${message}`);
+  });
+  socket.send(`Hi there, I'm a WebSocket server`);
 });
 
-
+// `server` is a vanilla Node.js HTTP server, so use
+// the same ws upgrade process described here:
+// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
 const server = app.listen(3000);
 server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, (socket) => {
-        wsServer.emit('connection', socket, request);
-    });
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request);
+  });
 });
