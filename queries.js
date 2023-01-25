31420bb1ab5061f1a148652b11f60c131e32d106
Skeegan123 <mcfeegan123@gmail.com>
2023-01-24 19:54:01 -0700

    removed stuff

--- queries.js
+++ queries.js
@@ -1,99 +1,128 @@
-const { json } = require('body-parser');
+const { json } = require("body-parser");
 
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'api',
-  password: '***REMOVED***',
+  user: "postgres",
+  host: "localhost",
+  database: "api",
+  password: "password",
   port: 5432,
 });
 
 const createUser = (request, response) => {
-    const { email } = request.body
-  
-    pool.query('INSERT INTO users (email) VALUES ($1) RETURNING *', [email], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).send(`User added with ID: ${results.rows[0].id}`)
-    })
-}
+  const { email } = request.body;
 
-const getUserById = (request, response) => {
-    const id = parseInt(request.params.id)
-  
-    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+  pool.query(
+    "INSERT INTO users (email) VALUES ($1) RETURNING *",
+    [email],
+    (error, results) => {
       if (error) {
-        throw error
+        throw error;
       }
-      response.status(200).json(results.rows)
-    })
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    }
+  );
 };
 
-  const updateUserPost = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { postID } = request.body
-  
-    pool.query(
-      'UPDATE users SET posts = array_append(posts, $1) WHERE id = $2',
-      [postID, id],
-      (error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).send(`User modified with ID: ${id}`)
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const updateUserPost = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { postID } = request.body;
+
+  pool.query(
+    "UPDATE users SET posts = array_append(posts, $1) WHERE id = $2",
+    [postID, id],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    )
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
 };
 
 const updateUserLike = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { postID } = request.body
-  
-    pool.query(
-      'UPDATE users SET likes = array_append(likes, $1) WHERE id = $2',
-      [postID, id],
-      (error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).send(`User modified with ID: ${id}`)
+  const id = parseInt(request.params.id);
+  const { postID } = request.body;
+
+  pool.query(
+    "UPDATE users SET likes = array_append(likes, $1) WHERE id = $2",
+    [postID, id],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    )
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
 };
 
 const createPost = (jsonFile) => {
-    readFile = JSON.parse(jsonFile, null, 2);
-    // console.log("" + jsonFile);
-    const { userID, coordinates, description, noise, crowd, idreq, wifi, amenities, tags, title } = readFile;
-  
-    pool.query('INSERT INTO posts ( userID, coordinates, description, noise, crowd, idreq, wifi, amenities, tags, title) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *', [userID, coordinates, description, noise, crowd, idreq, wifi, amenities, tags, title], (error, results) => {
+  readFile = JSON.parse(jsonFile, null, 2);
+  // console.log("" + jsonFile);
+  const {
+    userID,
+    coordinates,
+    description,
+    noise,
+    crowd,
+    idreq,
+    wifi,
+    amenities,
+    tags,
+    title,
+  } = readFile;
+
+  pool.query(
+    "INSERT INTO posts ( userID, coordinates, description, noise, crowd, idreq, wifi, amenities, tags, title) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+    [
+      userID,
+      coordinates,
+      description,
+      noise,
+      crowd,
+      idreq,
+      wifi,
+      amenities,
+      tags,
+      title,
+    ],
+    (error, results) => {
       if (error) {
         console.error(error);
-        throw error
+        throw error;
       }
-    //   response.status(201).send(`Post added with ID: ${results.rows[0].id}`)
-    })
+      //   response.status(201).send(`Post added with ID: ${results.rows[0].id}`)
+    }
+  );
 };
 
 function getPosts() {
-    pool.query('SELECT * FROM posts ORDER BY postID ASC;', (error, results) => {
-      if (error) {
-        throw error
-      }
+  pool.query("SELECT * FROM posts ORDER BY postID ASC;", (error, results) => {
+    if (error) {
+      throw error;
+    }
     //   response.status(200).json(results.rows)
     return results.rows;
-    });
-};
+  });
+}
 
 module.exports = {
-    getUserById,
-    createUser,
-    updateUserPost,
-    updateUserLike,
-    createPost,
-    getPosts,
-  }
-  
+  getUserById,
+  createUser,
+  updateUserPost,
+  updateUserLike,
+  createPost,
+  getPosts,
+};
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const nodemon = require('nodemon');
-const ws = require('ws');
-const db = require('./queries');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const nodemon = require("nodemon");
+const ws = require("ws");
+const db = require("./queries");
+const fs = require("fs");
 
 const app = express();
 
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'api',
-  password: '***REMOVED***',
+  user: "postgres",
+  host: "localhost",
+  database: "api",
+  password: "password",
   port: 5432,
 });
 
@@ -24,39 +24,45 @@ app.use(cors());
 
 pool.connect((err, client, release) => {
   if (err) {
-      return console.error(
-          'Error acquiring client', err.stack)
+    return console.error("Error acquiring client", err.stack);
   }
-  client.query('SELECT NOW()', (err, result) => {
-      release()
-      if (err) {
-          return console.error(
-              'Error executing query', err.stack)
-      }
-      console.log("Connected to Database !")
-  })
-})
+  client.query("SELECT NOW()", (err, result) => {
+    release();
+    if (err) {
+      return console.error("Error executing query", err.stack);
+    }
+    console.log("Connected to Database !");
+  });
+});
 
 // Set up a headless websocket server that prints any
 // events that come in.
 const wsServer = new ws.Server({ noServer: true });
-wsServer.on('connection', socket => {
-    socket.on('message', message => {
-      if (("" + message) === 'load') {
-        console.log('Loading posts');
-        pool.query('SELECT * FROM posts ORDER BY postID ASC;', (error, results) => {
+wsServer.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    if ("" + message === "load") {
+      console.log("Loading posts");
+      pool.query(
+        "SELECT * FROM posts ORDER BY postID ASC;",
+        (error, results) => {
           if (error) {
-            throw error
+            throw error;
           }
-        //   response.status(200).json(results.rows)
-        socket.send(JSON.stringify(results.rows));
-        });
-      } else {
+          //   response.status(200).json(results.rows)
+          socket.send(JSON.stringify(results.rows));
+        }
+      );
+    } else {
       // console.log("" + message);
       let post = JSON.parse(message);
       let postObject = {
         userID: 1,
-        coordinates: ( '(' + parseFloat(post.latitude) + ',' + parseFloat(post.longitude) +')' ),
+        coordinates:
+          "(" +
+          parseFloat(post.latitude) +
+          "," +
+          parseFloat(post.longitude) +
+          ")",
         description: post.locationDescription,
         noise: post.noiseLevel,
         crowd: post.crowdLevel,
@@ -65,10 +71,10 @@ wsServer.on('connection', socket => {
         amenities: post.amenities,
         tags: post.tags,
         title: post.locationName,
-      }
+      };
       db.createPost(JSON.stringify(postObject));
       socket.send(`Hello, you sent -> ${message}`);
-      }
+    }
   });
   // socket.send(`Hi there, I'm a WebSocket server`);
 });
@@ -77,8 +83,8 @@ wsServer.on('connection', socket => {
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
 const server = app.listen(3000);
-server.on('upgrade', (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, socket => {
-    wsServer.emit('connection', socket, request);
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
   });
 });
